5c14e62dd048b4839c5e33e7f1050355fcffdfea
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-01-22 12:30:11 +0100

    Loggiloggi fixifixi klart

--- logbook.php
+++ logbook.php
@@ -4,9 +4,7 @@ require('functions.php');
 require('hotelFunctions.php');
 
 $logbook = json_decode(file_get_contents(__DIR__ . "/logbook.json"), true);
-
 $bookings = 0;
-$featuresTimesUsed = [];
 ?>
 
 
@@ -25,10 +23,13 @@ $featuresTimesUsed = [];
 <body>
     <header>
         <div class="header-container">
-            <h1> Dino Resort Logbook</h1>
+            <h1><a href="./index.php"> Dino Resort Logbook</a></h1>
             <i>Yeah this idea has been tried before but this time it'll work out, promise!</i>
         </div>
-        <!-- <h2> Our rooms:</h2> -->
+        <div class="logbook-links">
+            <a href="./index.php">Index</a>
+            <a href="./logbook.php">Logbook</a>
+        </div>
     </header>
     <Main>
         <section class=cards-container>
@@ -37,18 +38,14 @@ $featuresTimesUsed = [];
             ?>
 
                 <div class="logbook-card">
-                    <h2>Island - <?= $booking["island"] ?></h2>
-                    <h3>Hotel - <?= $booking["hotel"] ?></h3>
+                    <h2>Island: <?= $booking["island"] ?></h2>
+                    <h3>Hotel: <?= $booking["hotel"] ?></h3>
                     <p>Duration: <?= $booking["arrival_date"] ?> - <?= $booking["departure_date"] ?> </p>
                     <p>Stars: <?= $booking["stars"] ?>
                     <p>Cost: <?= $booking["total_cost"] ?>
                     <h3>Features:</h3>
                     <?php foreach ($booking["features"] as $feature) :
-                        if (isset($featuresTimesUsed[$feature["name"]])) {
-                            $featuresTimesUsed[$feature["name"]] += 1;
-                        } else {
-                            $featuresTimesUsed[$feature["name"]] = 1;
-                        } ?>
+                    ?>
 
                         <p>Name: <?= $feature["name"] ?></p>
                         <p>Cost: <?= $feature["cost"] ?></p>
@@ -61,14 +58,8 @@ $featuresTimesUsed = [];
     </Main>
 
     <?php
-    $mostBookedFeature = "None";
-    $timesBooked = 0;
-    foreach ($featuresTimesUsed as $name => $times) {
-        if ($times > $timesBooked) {
-            $mostBookedFeature = $name;
-            $timesBooked = $times;
-        }
-    }
+
+
     $revenue = 0;
     $numberOfBookings = 0;
     $revenuePerBooking = 0;
@@ -80,23 +71,17 @@ $featuresTimesUsed = [];
             $numberOfBookings += 1;
         }
     }
-
-
     if ($numberOfBookings !== 0) $revenuePerBooking = ($revenue /  $numberOfBookings)
 
     ?>
 
+    <h2>Fact Box:</h2>
+    <section class="fact-box">
 
-    <section>
-        <h2>Fact Böx:</h2>
         <p>Total Revenue: <?= $revenue ?></p>
         <p>Revenue Per Booking: <?= number_format($revenuePerBooking, 2, ".") ?></p>
-        <p>Most Popular Feature: <?= $mostBookedFeature ?> (Booked <?= $timesBooked ?> times!)
-
-
         </p>
 
-        <!-- Revenue per booking -->
 
 
     </section>
